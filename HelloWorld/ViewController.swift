f30f6b4aec7fffd29bf4c1e3305231b2767d385b
Michael Nikitochkin <miry.sof@gmail.com>
2014-08-09 17:43:58 +0300

    :panda_face: Show Table with simple content

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
                             
+    @IBOutlet weak var appTableView: UITableView!
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel.text = "G Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
